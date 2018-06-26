e41796b5ade9b0a6b0c3820f6b1126abd38471cb
Ignat Bavtovich <ignatbav@gmail.com>
2018-06-26 12:26:24 +0300

    Stream Exercise 4 Part 2 added

--- src/test/java/streams/part2/exercise/Exercise4.java
+++ src/test/java/streams/part2/exercise/Exercise4.java
@@ -3,7 +3,11 @@ package streams.part2.exercise;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,12 +17,24 @@ public class Exercise4 {
      * Выбирает из текста наиболее часто встречающиеся слова.
      * Подсчет слов выполняется без учета их регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово.
      * Если некоторые слова имеют одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
-     * @param text Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
+     *
+     * @param text        Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
      * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо отобрать.
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+
+        return Arrays.stream(text.split("\\s"))
+                     .map(String::toLowerCase)
+                     .collect(Collectors.toMap(Function.identity(), i -> 1, (e1, e2) -> e1 + e2))
+                     .entrySet()
+                     .stream().sorted(Comparator.comparing((Function<Map.Entry<String, Integer>, Integer>) Map.Entry::getValue,
+                                                            (v1, v2) -> Integer.compare(v2, v1))
+                                                .thenComparing(Map.Entry::getKey, String::compareTo))
+                     .limit(numberWords)
+                     .map(Map.Entry::getKey)
+                     .collect(Collectors.toList());
+
     }
 
     @Test
@@ -33,13 +49,14 @@ public class Exercise4 {
     @Test
     public void test2() {
         String source = "Lorem ipsum dolor sit amet consectetur adipiscing elit Sed sodales consectetur purus at "
-                      + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
-                      + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
-                      + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
-                      + "Integer vel odio nec mi tempor dignissim";
+                + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
+                + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
+                + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
+                + "Integer vel odio nec mi tempor dignissim";
 
         List<String> result = getFrequentlyOccurringWords(source, 10);
 
-        assertEquals(Arrays.asList("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet", "dolor", "eget", "elit", "mi"), result);
+        assertEquals(Arrays.asList("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet", "dolor", "eget", "elit", "mi"),
+                result);
     }
 }
